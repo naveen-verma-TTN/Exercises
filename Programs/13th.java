39a99703afb427a967b2816139ae70ba098c9e05
naveen-verma-TTN <naveen.verma@tothenew.com>
2020-02-19 17:36:41 +0530

    Introduction to Java 2

--- Programs/13th.java
+++ Programs/13th.java
@@ -2,19 +2,30 @@
 class JavaException{
    public static void main(String args[]){
   try{
-       throw new myException("myException");
+       throw new myException("myException", true);
   }
  catch(myException e){
     System.out.println(e) ;
  }
 }
 }
-class myException extends Exception{
-    String s;
-   myException(String str) {
-     s = str;
-   }
-   public String toString(){
-     return ("Exception: " + s) ;
+
+
+class myException extends Exception {
+
+  private boolean suppressStacktrace = false;
+
+  public myException(String message, boolean suppressStacktrace) {
+      super(message, null, suppressStacktrace, !suppressStacktrace);
+      this.suppressStacktrace = suppressStacktrace;
+  }
+
+  @Override
+  public String toString() {
+      if (suppressStacktrace) {
+          return getLocalizedMessage();
+      } else {
+          return super.toString();
+      }
   }
 }
