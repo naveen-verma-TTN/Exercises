e6ea65fa534c4339b57a88ae97f7b975854c7668
naveen-verma-TTN <naveen.verma@tothenew.com>
2020-02-25 14:12:20 +0530

    Activity and Intents

--- app/src/main/java/com/example/activityandintents/MainActivity.java
+++ app/src/main/java/com/example/activityandintents/MainActivity.java
@@ -13,6 +13,21 @@ import com.google.android.material.snackbar.Snackbar;
 
 import java.util.regex.Pattern;
 
+
+/* Exercise
+
+1. Create one signup activity having “user name”, “email-id”, “phone number”, “password” with all fields mandatory.
+
+* Explicit Intent : On clicking on signup button user should able to see all the filled details on the next screen.
+
+* Implicit Intent : On the second Activity there should be a input box and two button along with the above details.
+  the user should be able to enter a website url and on clicking one of the button an implicit intent should be created
+  to open a web browser with the url entered by the user.
+
+* Permission : On clicking on the second button check whether the app has the camera permission granted
+  or not if permission is granted display permission is granted otherwise ask user for permission and display
+  whether the permission is granted or not.
+ */
 public class MainActivity extends AppCompatActivity {
     EditText editText_username;
     EditText editText_Phoneno;
